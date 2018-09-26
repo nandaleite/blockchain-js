cdcf200ceafafdce4702770b5283620ff28cc7e4
Fernanda Leite <contato@nandaleite.com>
2018-09-26 20:33:12 -0300

    Add createNewBlock method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,24 @@
 class Blockchain {
   constructor() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
+  }
+
+  createNewBlock(nonce, previousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.pendingTransactions,
+      nonce: nonce,
+      hash: hash,
+      previousBlockHash: previousBlockHash
+    };
+
+    this.pendingTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
   }
 }
+
+module.exports = Blockchain;
